1a7f10a46c2242504ae5fa86fe7b39c57004c568
pierce <pierce@vulpes.dev>
2026-05-11 23:25:47 -0400

    indentation

--- lua/includes/modules/hook.lua
+++ lua/includes/modules/hook.lua
@@ -216,11 +216,11 @@ local function hookRemove( event_name, name )
         event[pos + 3] = nil --[[priority]]
     end
 
-	local ulibEventHooks = ulibHooks[event_name]
-	if ulibEventHooks then
-		for _, hooks in pairs(ulibEventHooks) do
-			hooks[name] = nil
-		end
+    local ulibEventHooks = ulibHooks[event_name]
+    if ulibEventHooks then
+        for _, hooks in pairs(ulibEventHooks) do
+            hooks[name] = nil
+        end
 	end
 
     events[event_name] = copy_event( event, event_name )
